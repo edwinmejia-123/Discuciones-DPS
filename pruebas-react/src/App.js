0db792784daba023db14785e5cce80b855c4405a
edwinmejia-123 <edwin_adonay123@hotmail.com>
2023-08-18 08:13:57 -0600

    Creacion de proyecto de prueba react, configuracion

---
+++ pruebas-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+return (
+<div className="App">
+<div className="App-content">
+<p>
+Aquí haremos nuestro TO-DO list
+</p>
+</div>
+</div>
+);
+}
+export default App
