dfd4777624ac0bc491df3e6d0fd402743ebfad30
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2023-08-18 12:46:00 -0600

    Correccion de errores

--- pruebas-react/src/App.js
+++ pruebas-react/src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import './App.css';
-import Form from './components/Form'
+import React from "react";
+import "./App.css";
+import Form from "./components/Form";
 
 const App = () => {
-return (
-<div className="App">
-<div className="App-content">
-<p>
-Aquí haremos nuestro TO-DO list
-</p>
-<Form />
-</div>
-</div>
-);
-}
-export default App
+  return (
+    <div className="App">
+      <div className="App-content">
+        <p>Aquí haremos nuestro TO-DO list</p>
+        <Form />
+      </div>
+    </div>
+  );
+};
+export default App;
