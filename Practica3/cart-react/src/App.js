15f98bc70c047e4b06b799cd93239db360ab673c
edwinmejia-123 <edwin_adonay123@hotmail.com>
2023-08-25 09:00:28 -0600

    Practica 3

---
+++ Practica3/cart-react/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { ProductList } from "./components/ProductList";
+import { Headers } from "./components/Headers";
+
+function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+  return (
+    <>
+      <Headers
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ProductList
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+    </>
+  );
+}
+
+export default App;
